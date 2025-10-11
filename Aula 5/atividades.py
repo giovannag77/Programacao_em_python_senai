3ffe6378726b0079913071afa9564ba845ff6391
Giovanna Godoi <gioannagodoi2007@gmail.com>
2025-10-11 15:02:29 -0300

    Add files via upload

---
+++ Aula 5/atividades.py
@@ -0,0 +1,64 @@
+#atividade 1
+
+# print('Olá, Seja bem-vindo!')
+
+#atividade 2
+
+# var = True
+# print (type (var))
+
+#atividade 3
+
+# n1 = 3.8
+# n2 = 2.2
+# print (float(n1 * n2))
+
+#atividade 4
+
+# n1= 6
+# n2= 2
+# print (int(n1 / n2))
+
+#atividade 5
+
+# n1= 5
+# n2= 8
+# print (int(n1 - n2))
+
+#atividade 6
+
+# n1= 25
+# n2= 5
+# print (int(n1 // n2))
+
+#atividade 7
+
+# n1 = 3.8
+# n2 = 2.2
+# n3 = 2.8
+# n4 = 3.6
+
+# print (float(n1 * n2 * n3 * n4))
+
+#atividade 8
+
+# n1 = 2
+# print('O dobro do seu número é' , n1 *2)
+
+#atividade 9
+
+# nome = input('Digite seu nome: ')
+# idade = int(input('Digite sua idade: '))
+# endereço = input('Digite seu endereço: ')
+# curso = input('Digite o seu curso: ')
+# salario = float(input('Digite seu salário: '))
+
+# print ('Nome: ',nome)
+# print ('Idade: ',idade)
+# print ('Endereço: ' ,endereço)
+# print ('Curso: ' ,curso)
+# print ('Salário: ' ,salario)
+
+var 6 =20
+
+print (var 6)
