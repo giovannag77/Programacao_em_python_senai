da4bdd2d2eff52571f5cee651423b8e2a9fbec0f
Giovanna Godoi <gioannagodoi2007@gmail.com>
2025-10-11 17:51:50 -0300

    Add files via upload

---
+++ Aula 7/exercicios.py
@@ -0,0 +1,21 @@
+#exercicio 0
+# lista  =  []
+
+# # l =  list(range(1,2000))
+# # print(l)
+
+# l = list(range(2,20,2))
+
+# print (l)
+
+#exercicio 1
+
+numeros= [1,2,3,4,5,6,7,8,9,10]
+
+
+#exercicio 2
+
+print(numeros[2])
+
+
+#exercicio 3
---
+++ Aula 7/sistema_de_notas.py
@@ -0,0 +1,40 @@
+# lista_alunos =[]
+# nome1 = input('Nome 1:')
+# nome2 = input('Nome 2:')
+# nome3 = input('Nome 3:')
+
+# lista_alunos.append(nome1)
+# lista_alunos.append(nome2)
+# lista_alunos.append(nome3)
+
+# notas_aluno1 =[float(input(f'nota 1: {nome1}')),float(input(f'Nota 2: {nome1}'))]
+# notas_aluno2 =[float(input(f'nota 1: {nome2}')),float(input(f'Nota 2: {nome2}'))]
+# notas_aluno3 =[float(input(f'nota 1: {nome3}')),float(input(f'Nota 2: {nome3}'))]
+
+# media_aluno1 = sum(notas_aluno1)/len(notas_aluno1)
+# media_aluno2 = sum(notas_aluno2)/len(notas_aluno2)
+# media_aluno3 = sum(notas_aluno3)/len(notas_aluno3)
+
+# print('Média alunos')
+
+# print (f'''
+
+# Médias: 
+# aluno {nome1} - {media_aluno1}
+# aluno {nome2} - {media_aluno2}
+# aluno {nome3} - {media_aluno3}
+
+# ''')
+
+# aprovado_1 = media_aluno1 >= 7
+# aprovado_2 = media_aluno2 >= 7
+# aprovado_3 = media_aluno3 >= 7
+
+# print(f'''
+# {nome1} - APROVADO - {aprovado_1}
+# {nome2} - APROVADO - {aprovado_2}
+# {nome3} - APROVADO - {aprovado_3}
+# ''')
+
+l = list(range(2,20))
+print (l)
