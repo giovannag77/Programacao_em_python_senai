1065c91467c936bc30349318f50deadeb0aa0df3
Giovanna Godoi <gioannagodoi2007@gmail.com>
2025-11-01 17:50:23 -0300

    Add files via upload

---
+++ aula 9/aula.py
@@ -0,0 +1,18 @@
+# numero = int(input('numero '))
+
+# match numero:
+#     case numero if numero % 2 == 0:
+#         print('Par')
+#     case _:    
+#         print('impar')
+
+var = input('insira algo: ')
+
+match var:
+    case  '':
+        	print('string vazia')
+
+    case _:
+        print('a string tem valor')
+
+
