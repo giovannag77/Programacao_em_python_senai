9e7d1f0dc91d091e41011a0d97257b19a64c3dfc
Giovanna Godoi <gioannagodoi2007@gmail.com>
2025-11-01 14:09:11 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,32 @@
+#exercicio 0
+
+lista = list(range(2,21,2))
+print(lista)
+
+#exercicio 1
+
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+#exercicio 2
+
+print(numeros[2])
+
+#exercicio 3
+
+numeros.append(9)
+print(numeros)
+
+#ecercicio 4
+
+numeros.remove(5)
+print(numeros)
+
+#exercico 5
+
+carros = ['Trax', 'Nivus','Astra']
+
+
+print(numeros,carros)
+
+
