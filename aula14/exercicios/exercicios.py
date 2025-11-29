e2d741e186b35c3c5d7b9076a1680261244fd7bb
Giovanna Godoi <gioannagodoi2007@gmail.com>
2025-11-29 10:38:02 -0300

    Add files via upload

---
+++ aula14/exercicios/exercicios.py
@@ -0,0 +1,21 @@
+import os
+
+# with open('novo_arquivo', 'r') as arquivo:
+#     conteudo = arquivo.read()
+#     print(conteudo)
+
+# with open('novo_diretorio', 'w') as novo_arquivo:
+#     os.mkdir('novo diretorioooo')
+
+# os.rename('novo diretoriooooo', 'aaaaa')
+
+# with os.scandir('aaaaa') as entrada:
+#     for arquivo in entrada:
+#         if arquivo.is_file():
+#             print(f'Arquivo encontrado: {arquivo.name}')
+
+import shutil
+
+# shutil.copytree('aaaaa','novo_arquivo')
+
+shutil.rmtree('novo_arquivo','novo_arquivo')
